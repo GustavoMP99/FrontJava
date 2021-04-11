c296088841b9dffddf420648e7ea75c0048e4153
Luis Gustavo Méndez Poveda <gustavo125.cr@gmail.com>
2021-04-10 21:30:00 -0600

    Terminal

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,16 +6,35 @@ import { ROUTES } from './app.routing';
 import { DataService } from './data.service';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { TerminalComponent } from './terminal/terminal.component';
 
+import { MatSlideToggleModule  } from '@angular/material/slide-toggle';
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
+import { NgTerminalModule } from 'ng-terminal';
+import { ReactiveFormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatCardModule } from '@angular/material/card';
+import { NgxJsonViewerModule } from 'ngx-json-viewer';
+import {MatFormFieldModule} from '@angular/material/form-field';
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    TerminalComponent
   ],
   imports: [
     BrowserModule,
     RouterModule.forRoot(ROUTES, {onSameUrlNavigation: 'reload'}),
-
+    NgTerminalModule,
+    MatButtonModule,
+    MatSlideToggleModule,
+    MatInputModule,
+    ReactiveFormsModule,
+    BrowserAnimationsModule,
+    MatCardModule,
+    NgxJsonViewerModule,
+    MatFormFieldModule
   ],
   providers: [ DataService ],
   bootstrap: [AppComponent]
--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,9 +1,14 @@
 import {Routes} from '@angular/router';
+import { from } from 'rxjs';
 import { HomeComponent } from './home/home.component';
+import {TerminalComponent} from './terminal/terminal.component';
 
 export const ROUTES: Routes = [
     {
         path: '',  component: HomeComponent
+    },
+    {
+        path: 'a',  component: TerminalComponent
     },
     
 ];
