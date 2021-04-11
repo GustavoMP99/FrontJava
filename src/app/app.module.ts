7481914b498b82ff123c7fe6861c34acfea48344
Merelyn Rodríguez Rojas <merelynrodriguezr@gmail.com>
2021-04-10 21:38:47 -0600

    Conexión con back

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { ROUTES } from './app.routing';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 
 import { DataService } from './data.service';
 import { AppComponent } from './app.component';
@@ -10,14 +11,15 @@ import { HomeComponent } from './home/home.component';
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
   ],
   imports: [
     BrowserModule,
     RouterModule.forRoot(ROUTES, {onSameUrlNavigation: 'reload'}),
+    HttpClientModule,
 
   ],
-  providers: [ DataService ],
+  providers: [ DataService, ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
